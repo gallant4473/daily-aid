9d2b817c513912f0f8f72bf4574b8991fa798e1e
Kv Kishore <kishore.kanala@innoplexus.com>
2018-08-12 15:44:44 +0530

    experiment

--- src/app/components/Premium/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Premium = () => (
-  <div>
-    hello
-  </div>
-)
-
-export default Premium
--- src/app/components/index.js
+++
@@ -1,9 +0,0 @@
-import NoDataFound from './NoDataFound'
-import Premium from './Premium'
-import Loader from './Loader'
-
-export {
-  NoDataFound,
-  Premium,
-  Loader
-}
